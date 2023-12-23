e0220f5adfd5eff5f5d36189e411922c0736f53b
Billy Joe Santos <santos16426@gmail.com>
2023-12-24 03:08:00 +0800

    style: removed container className

--- src/app/components/Layout/index.tsx
+++ src/app/components/Layout/index.tsx
@@ -1,18 +1,27 @@
 import React, { ReactElement, ReactNode } from "react";
+import { Inter } from 'next/font/google'
 import Header from "../Header";
+import "@/app/styles/globals.scss";
+import { Metadata } from "next";
+import { cn } from "@/lib/utils";
 
-interface LayoutProps {
-  children: ReactNode | ReactElement[] | ReactNode;
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Lucas | Software Engineer',
+  description: 'Website for Billy Joe Santos'
 }
-const Layout:React.FC<LayoutProps> = ({ children }) => {
-
+export default function RootLayout({
+  children,
+}:{
+  children: React.ReactNode
+}){
   return(
-    <>
-      <Header/>
-      <main>{children}</main>
-    </>
+      <div className={cn("relative h-full font-sans antialiased", inter.className)}>
+          <Header/>
+          <main>{children}</main>
+      </div>
   )
 };
 
 
-export default Layout;
---
+++ src/app/components/MaxWidthWrapper.tsx
@@ -0,0 +1,11 @@
+// const MaxWidthWrapper = ({
+//     className,
+//     children,
+// } : {
+//     className? : string,
+//     children: ReactNode
+// }) => (
+//         <div className={cn() }>
+//             {children}
+//         </div>
+//     )
