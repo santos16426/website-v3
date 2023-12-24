5f29d7850b578fc7a1318b7087873003541c8c4e
Billy Joe Santos <santos16426@gmail.com>
2023-12-24 23:36:19 +0800

    refactor: updated styles, removed unnecessary scss, used tailwind classes

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -4,13 +4,13 @@ interface LogoProps{
     onClick: MouseEventHandler;
 }
 const Logo:React.FC<LogoProps> = ({onClick}) => ( 
-    <a href="#home" onClick={onClick}>
+    <Link href="#home" onClick={onClick}>
         <div className="flex items-center text-3xl font-bold cursor-pointer">
             <span className="text-orange-500">&lt;</span>
             <span className="text-black-500 hidden sm:flex">Billy</span>
             <span className="text-orange-500">/&gt;</span>
         </div>
-    </a>
+    </Link>
 );
 
 export default Logo;
--- src/app/components/Typewriter.tsx
+++ src/app/components/Typewriter.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Typewriter from "typewriter-effect";
 import styles  from "@/app/styles/Typewriter.module.scss";
+import { cn } from "@/lib/utils";
 interface BannerStringProps{
   skills : string[]
 }
 const BannerString:React.FC<BannerStringProps> = ({ skills }) => (
-  <div className={styles.wrapper}>
+  <div className={cn('text-left text-black text-3xl md:text-4xl', styles.wrapper)}>
     <Typewriter
       options={{
         strings: skills,
