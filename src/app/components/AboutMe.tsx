3eacaf65338db732b1a512263a023124cdf4bedc
Billy Joe Santos <santos16426@gmail.com>
2023-12-26 23:31:28 +0800

    style: updated mobile responsive

--- src/app/components/AboutMe.tsx
+++ src/app/components/AboutMe.tsx
@@ -3,8 +3,8 @@ import GridItem from "./GridItem";
 
 const AboutMe = () => {
     return(
-        <div className='relative w-full h-screen bg-muted overflow-y-auto'>
-            <div className='hidden sm:grid  w-full h-full p-40 grid-cols-4 auto-rows-[90px] gap-4'>
+        <div className='relative w-full h-screen bg-muted overflow-y-auto  items-center flex flex-row justify-center '>
+            <div className='hidden sm:grid  w-full h-full p-40 grid-cols-4 auto-rows-[90px] gap-4  max-w-[1920px]'>
                 {DesktopGridItems && DesktopGridItems.map((item,index)=>{
                     return<GridItem key={index} size={item.layout}>{item.children &&  item.children}</GridItem>
                 })}
--- src/app/components/GridItem.tsx
+++ src/app/components/GridItem.tsx
@@ -6,13 +6,13 @@ const variants = cva(
     {
        variants: {
         size: {
-            "1x1" : "col-span-1 row-span-1",
-            "1x2" : "col-span-1 row-span-2",
-            "2x1" : "col-span-2 row-span-1",
-            "2x2" : "col-span-2 row-span-2",
-            "2x3" : "col-span-2 row-span-3",
+            "1x1" : "col-span-1 row-span-1 max-w-[388px]",
+            "1x2" : "col-span-1 row-span-2 max-w-[388px]",
+            "2x1" : "col-span-2 row-span-1 max-w-[792px]",
+            "2x2" : "col-span-2 row-span-2 max-w-[792px]",
+            "2x3" : "col-span-2 row-span-3 max-w-[792px]",
             "4x2" : "col-span-4 row-span-2",
-            "2x4" : "col-span-2 row-span-4",
+            "2x4" : "col-span-2 row-span-4 max-w-[792px]",
             "4x4" : "col-span-4 row-span-4",
         }
         },
