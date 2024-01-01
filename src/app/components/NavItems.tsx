fd51ece606a1e108260d903bf38f3f095ed1ade0
Billy Joe Santos <santos16426@gmail.com>
2024-01-02 00:38:54 +0800

    style: mobile header

--- src/app/components/NavItems.tsx
+++ src/app/components/NavItems.tsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import styles from '@/app/styles/NavItems.module.scss'
 import { cn } from "@/app/lib/utils";
-import { Menu } from "lucide-react";
+import { Menu, X } from "lucide-react";
+import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from "./ui/drawer";
+import Logo from "./Logo";
 
 
 
@@ -48,7 +50,63 @@ const NavItems = () => {
                 Contact
             </a>
         </nav>
-        <Menu className="block sm:hidden text-gray-700 cursor-pointer" onClick={()=>setIsDrawerOpen(true)}/>
+        
+        <Drawer open={isDrawerOpen}>
+            <DrawerTrigger><Menu className="block sm:hidden text-gray-700 cursor-pointer" onClick={()=>setIsDrawerOpen(true)}/></DrawerTrigger>
+            <DrawerContent>
+                <div className="h-screen text-black flex flex-col justify-center items-center text-center">
+                    <nav className="flex flex-col justify-center items-center text-center gap-10">
+                            <Logo  onClick={() =>{ 
+                                scrollToSection("1")
+                                setIsDrawerOpen(false)
+                            }}/>
+                        <a
+                            href="#about"
+                            onClick={() => {
+                                scrollToSection("2")
+                                setTimeout(()=>setIsDrawerOpen(false),250)
+                            }}
+                            className={cn('relative text-gray-500',styles.navLink)}
+                        >
+                            About
+                        </a>
+                            <a
+                                href="#experience"
+                                onClick={() => {
+                                    scrollToSection("3")
+                                    setTimeout(()=>setIsDrawerOpen(false),250)
+                                }}
+                                className={cn('relative text-gray-500',styles.navLink)}
+                            >
+                                Experience
+                            </a>
+                            <a
+                                href="#projects"
+                                onClick={() => {
+                                    scrollToSection("4")
+                                    setTimeout(()=>setIsDrawerOpen(false),250)
+                                }}
+                                className={cn('relative text-gray-500',styles.navLink)}
+                            >
+                                Projects
+                            </a>
+                            <a
+                                href="#contact"
+                                onClick={() => {
+                                    scrollToSection("5")
+                                    setTimeout(()=>setIsDrawerOpen(false),250)
+                                }}
+                                className={cn('relative text-gray-500',styles.navLink)}
+                            >
+                                Contact
+                            </a>
+                        <DrawerFooter>
+                                <X className="text-gray-500" onClick={()=>setIsDrawerOpen(false)}/>
+                        </DrawerFooter>
+                    </nav>
+                </div>
+            </DrawerContent>
+        </Drawer>
     </>
         
     )
