7f8fa206669a661e63e140fe352240d013caa01e
Billy Joe Santos <santos16426@gmail.com>
2024-01-26 20:51:14 +0800

    fixed: types for exppage

--- src/app/components/Experience.tsx
+++ src/app/components/Experience.tsx
@@ -1,6 +1,4 @@
 import { experiences } from '../config/experiences'
-import { cn } from '../lib/utils'
-
 const Experience = () => {
   return (
     <div className="w-full h-full flex flex-col bg-[#EFEEEF] py-12">
@@ -16,27 +14,37 @@ const Experience = () => {
           <div className="w-full flex lg:hidden flex-row border-b-[1px] border-gray-300 px-8">
             <div className="w-full">Company & Client/Details</div>
           </div>
-          {experiences.map((exp) => (
-            <div
-              className="relative w-full group flex flex-col lg:flex-row border-b-[1px] border-gray-300 px-8 py-12 text-lg hover:bg-gray-200 "
-              key={exp.key}
-            >
-              <div className="w-full z-10 text-2xl">{exp.name}</div>
-              <div className="w-full flex flex-col lg:flex-row z-10">
-                <div className="flex flex-col w-full">
-                  <p className="font-[500]">{exp.details.title}</p>
-                  <p className="text-gray-400">{exp.details.year}</p>
-                </div>
-                <div className="flex flex-col w-full">
-                  <ul className="lg:list-disc mt-4 lg:m-0">
-                    {exp.details.jobDescription.map((desc, index) => (
-                      <li key={index}>{desc}</li>
-                    ))}
-                  </ul>
+          {experiences.map(
+            (exp: {
+              key: string
+              name: string
+              details: {
+                title: string
+                year: string
+                jobDescription: string[]
+              }
+            }) => (
+              <div
+                className="relative w-full group flex flex-col lg:flex-row border-b-[1px] border-gray-300 px-8 py-12 text-lg hover:bg-gray-200 "
+                key={exp.key}
+              >
+                <div className="w-full z-10 text-2xl">{exp.name}</div>
+                <div className="w-full flex flex-col lg:flex-row z-10">
+                  <div className="flex flex-col w-full">
+                    <p className="font-[500]">{exp.details.title}</p>
+                    <p className="text-gray-400">{exp.details.year}</p>
+                  </div>
+                  <div className="flex flex-col w-full">
+                    <ul className="lg:list-disc mt-4 lg:m-0">
+                      {exp.details.jobDescription.map((desc: string, index) => (
+                        <li key={index}>{desc}</li>
+                      ))}
+                    </ul>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ),
+          )}
         </div>
       </div>
     </div>
