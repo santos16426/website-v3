6cfff0fda75fc681889a572f1611fbd0fe9cee09
Billy Joe Santos <santos16426@gmail.com>
2024-01-28 02:03:41 +0800

    feature:project page

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { projects } from '../config/projects'
 import { cn } from '../lib/utils'
 
@@ -17,48 +18,54 @@ const Projects = () => {
             <div className="w-full">Projects/Details</div>
           </div>
           {projects.map((project) => (
-            <div
-              className="relative w-full group flex flex-col lg:flex-row border-b-[1px] border-gray-300 px-4 lg:px-8 py-12 text-2xl hover:text-white cursor-pointer select-none"
+            <Link
+              href="/project/[projectKey]"
+              as={`/project/${project.key}`}
               key={project.key}
             >
-              {/* image */}
               <div
-                className={cn(
-                  'scale-0 transition-all lg:group-hover:scale-100 fixed bottom-10 right-10 shadow-2xl h-[400px] z-[100] aspect-video bg-red-500',
-                  { grayscale: project.disabled },
-                )}
-                style={{
-                  backgroundImage: `url(${project.image})`,
-                  backgroundSize: 'cover',
-                }}
+                className="relative w-full group flex flex-col lg:flex-row border-b-[1px] border-gray-300 px-4 lg:px-8 py-12 text-2xl hover:text-white cursor-pointer select-none"
+                key={project.key}
               >
-                {project.disabled && (
-                  <div className="w-full h-full header bg-black bg-opacity-80 font-bold text-8xl whitespace-nowrap grid place-items-center">
-                    <p className="">Coming Soon</p>
-                  </div>
-                )}
+                {/* image */}
+                <div
+                  className={cn(
+                    'scale-0 transition-all lg:group-hover:scale-100 fixed bottom-10 right-10 shadow-2xl h-[400px] z-[100] aspect-video bg-red-500',
+                    { grayscale: project.disabled },
+                  )}
+                  style={{
+                    backgroundImage: `url(${project.image})`,
+                    backgroundSize: 'cover',
+                  }}
+                >
+                  {project.disabled && (
+                    <div className="w-full h-full header bg-black bg-opacity-80 font-bold text-8xl whitespace-nowrap grid place-items-center">
+                      <p className="">Coming Soon</p>
+                    </div>
+                  )}
+                </div>
+                {/* project list */}
+                <div className="w-full z-10">
+                  {project.name}
+                  {project.disabled && (
+                    <span className="text-orange-400 group-hover:text-orange-600">
+                      {' '}
+                      (Coming Soon)
+                    </span>
+                  )}
+                </div>
+                <div className="w-full flex flex-row z-10 lg:text-black text-gray-400 group-hover:text-white group-hover:opacity-70">
+                  {project.details.type}
+                </div>
+                {/*  bgHover */}
+                <div
+                  className={cn(
+                    'absolute top-1/2 -translate-y-1/2 left-0 bg-orange-500 w-full h-0 transition-all group-hover:h-full duration-500',
+                    { 'bg-gray-400': project.disabled },
+                  )}
+                />
               </div>
-              {/* project list */}
-              <div className="w-full z-10">
-                {project.name}
-                {project.disabled && (
-                  <span className="text-orange-400 group-hover:text-orange-600">
-                    {' '}
-                    (Coming Soon)
-                  </span>
-                )}
-              </div>
-              <div className="w-full flex flex-row z-10 lg:text-black text-gray-400 group-hover:text-white group-hover:opacity-70">
-                {project.details.type}
-              </div>
-              {/*  bgHover */}
-              <div
-                className={cn(
-                  'absolute top-1/2 -translate-y-1/2 left-0 bg-orange-500 w-full h-0 transition-all group-hover:h-full duration-500',
-                  { 'bg-gray-400': project.disabled },
-                )}
-              />
-            </div>
+            </Link>
           ))}
         </div>
       </div>
