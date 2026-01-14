083ac2fb34708327842a767279c97ebd13da7f0b
Billy Joe Santos <santos16426@gmail.com>
2026-01-15 03:11:36 +0800

    feat: google ownership

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,22 @@
-import Head from 'next/head'
-import { NextComponentType } from 'next'
-import '@/app/styles/globals.scss'
-import { AppProps } from 'next/app'
+import Head from "next/head";
+import { NextComponentType } from "next";
+import "@/app/styles/globals.scss";
+import { AppProps } from "next/app";
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
       <Head>
         <title>Lucas | Software Engineer</title>
-        <link rel="icon" href="/images/icons/logo.svg" type="image/svg+xml" />
+        <link rel='icon' href='/images/icons/logo.svg' type='image/svg+xml' />
+        <meta
+          name='google-site-verification'
+          content='WIuJFRN2EcEe7Vk-pU2GhsSYylgHo-WoR4WNqrT6lqs'
+        />
       </Head>
       <Component {...pageProps} />
     </>
-  )
-}
+  );
+};
 
-export default MyApp
+export default MyApp;
